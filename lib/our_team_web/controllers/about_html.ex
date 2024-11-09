9188875ad98f9a1ecc5749ec04a2178208293b83
srujan <srujan.reddy@grnconnect.com>
2024-11-10 01:31:11 +0530

    Added setup

--- lib/our_team_web/controllers/about_html.ex
+++ lib/our_team_web/controllers/about_html.ex
@@ -1,11 +1,9 @@
 defmodule OurTeamWeb.AboutHTML do
-  @moduledoc """
-  This module contains pages rendered by PageController.
-
-  See the `page_html` directory for all templates available.
-  """
   use OurTeamWeb, :html
 
-  # embed_templates "about_html/*"
+
+
+  embed_templates "about_html/*"
+
 
 end
