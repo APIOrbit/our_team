8ef29854ce1cb3e918d7de56f8a6cd7ac9c7d6c5
Srujan Reddy <32371403+singlefleck@users.noreply.github.com>
2024-11-10 01:49:42 +0530

    4 basic setup (#5)
    * Added setup
    
    * chore: added new items
    
    ---------
    
    Co-authored-by: srujan <srujan.reddy@grnconnect.com>

--- lib/our_team_web/controllers/about_controller.ex
+++ lib/our_team_web/controllers/about_controller.ex
@@ -1,13 +1,7 @@
 defmodule OurTeamWeb.AboutController do
   use OurTeamWeb, :controller
 
-  alias OurTeamWeb.AboutHTML
-
   def about(conn, _params) do
-
-    conn = put_view(conn, html: AboutHTML)
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :about, layout: false)
+    render(conn, :about)
   end
 end
--- lib/our_team_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule OurTeamWeb.PageController do
-  use OurTeamWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
